aa33c0bb1fbc7693ea9c9f67200b5449f8fe0193
Paulo <pmatheus.nsx@gmail.com>
2022-10-10 21:21:49 -0300

    porthost

--- flare/analytics/command_control.py
+++ flare/analytics/command_control.py
@@ -153,9 +153,9 @@ class elasticBeacon(object):
         try:
             self.vprint('{info}[INFO]{endc} Attempting to connect to elasticsearch...'.format(info=bcolors.OKBLUE, endc=bcolors.ENDC))
             if self.auth == "None":
-                self.es = Elasticsearch(f"{self.es_host}:{self.es_port}", timeout=self.es_timeout, verify_certs=False)
+                self.es = Elasticsearch(f"http://{self.es_host}:{self.es_port}", timeout=self.es_timeout, verify_certs=False)
             else:
-                self.es = Elasticsearch(f"{self.es_host}:{self.es_port}", timeout=self.es_timeout, http_auth=(self.auth_user, self.auth_password), verify_certs=False)
+                self.es = Elasticsearch(f"http://{self.es_host}:{self.es_port}", timeout=self.es_timeout, http_auth=(self.auth_user, self.auth_password), verify_certs=False)
             self.vprint('{green}[SUCCESS]{endc} Connected to elasticsearch on {host}:{port}'.format(green=bcolors.OKGREEN, endc=bcolors.ENDC, host=self.es_host, port=str(self.es_port)))
         except Exception as e:
             self.vprint(e)
