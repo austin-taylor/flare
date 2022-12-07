1865b8501cc1119a750df4e818cd660357bd2dcc
Paulo <pmatheus@gmail.com>
2022-12-07 11:17:29 -0300

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Flare',
-    version='0.9',
+    version='0.91',
     platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
     license="""MIT License
 
